aaccec029978a51af201c8c1f35cf210ee508050
ryuparish <ryuparish1115@gmail.com>
2021-06-22 16:33:34 -0500

    Added incomplete queue_via_stack

--- Stacks_and_Queues/queue_via_stack.cpp
+++ Stacks_and_Queues/queue_via_stack.cpp
@@ -1,55 +1,53 @@
 #include <iostream>
+#include <assert.h>
 // Apprently the solution is to have a second stack that takes all the nodes popped when getting the last element from the primary stack. Then the secondary stack loads all the nodes back onto the
 // primary stack without the last element from the original primary stack.
 
+///// LINK DATA STRUCTURE RULES //////
+// 1. Always use "new" when creating pointers that aren't immediately
+// assigned to another pointer's object.
+
 struct stackNode{
-    int min_value, value;
+    int value;
     stackNode* next = NULL;
 };
 
 struct Stack{
     stackNode* top = new stackNode;
-    // Constructor
-    Stack(){
-        top->value = 0;
-        top->min_value = 0;
-    }
-    
-    void addNode(int nodeValue){
+
+    void push(int nodeValue){
         stackNode* newNode = new stackNode;
         newNode->value = nodeValue;
-        // If this is the first value to be added to the stack
-        if(top->next == NULL){
-            newNode->min_value = nodeValue;
-            newNode->next = top;
-            top = newNode;
-        }
-        else{
-            if(nodeValue < top->min_value){
-                newNode->min_value = nodeValue;
-            }
-            else{newNode->min_value = top->min_value;}
-            newNode->next = top;
-            top = newNode;
-        }
+        newNode->next = top;
+        top = newNode;
+        
     }
 
     // Make this so that it returns the node that is to be deleted
-    void deleteNode(){
+    stackNode* pop(){
         if(top->next != NULL){
-            stackNode* purger = top;
+            stackNode* popped_node = new stackNode;
+            popped_node->value = top->value;
             top = top->next;
-            free(purger);
+            return popped_node;
         }
-    }
-    
-    int minval(){
-        return top->min_value;
+        return NULL;
     }
 
+    stackNode* peek(){
+        return top;
+    }
+
+    bool isEmpty(){
+        return top->next == NULL;
+    }
+    
     ~Stack(){
-        stackNode* purger;
-        stackNode* walker = top->next;
+        stackNode* purger = NULL;
+        stackNode* walker = NULL;
+        if(top->next != NULL){
+            stackNode* walker = top->next;
+        }
         free(top);
         while(walker != NULL){
             purger = walker;
@@ -57,9 +55,39 @@ struct Stack{
             free(purger);
         }
     }
+};
 
-        
+struct myQueue{
+    Stack* stack1 = NULL;
+    Stack* stack1_reverse = NULL;
 
+    myQueue(Stack*& orderedStack, Stack*& reversedStack){
+        stack1 = orderedStack;
+        stack1_reverse = reversedStack;
+    }
+
+    void unloadStack1(){
+        stackNode* nodeHolder;
+        int intHolder;
+        while(!stack1->isEmpty()){
+            nodeHolder = stack1->pop();
+            intHolder = nodeHolder->value;
+            free(nodeHolder);
+            stack1_reverse->push(intHolder);
+        }
+    }
+
+    void push(int nodeValue){
+        stack1->push(nodeValue);
+    }
+
+    stackNode* pop(){
+        stackNode* poppedNode = stack1_reverse->pop();
+        if(stack1_reverse->isEmpty()){
+            unloadStack1();
+        }
+        return poppedNode;
+    }
 };
 
 void print_stack(Stack*& myStack){
@@ -73,37 +101,21 @@ void print_stack(Stack*& myStack){
     std::cout << "\n";
 }
 
+// The only time we want to unload stack1 onto stack 2 is when stack 2 is empty
 int main(){
-    Stack* myStack = new Stack;
-    int value_cell, flip = 1;
-    char a;
-    while(flip){
-        print_stack(myStack);
-        std::cout << "Enter a to add a stack node\nEnter d to pop from the stack\nEnter m to see the minimum value\nEnter q to quit\n";
-        std::cin >> a;
-        while(a != 'q'){
-            if(a == 'a'){
-                std::cout << "What value would you like to add? ";
-                std::cin >> value_cell;
-                myStack->addNode(value_cell);
-            }
+    int a;
+    std::cout << "Enter the numbers you want to push onto the stack\n";
+    Stack* stack1 = new Stack;
+    Stack* stack2 = new Stack;
+    myQueue* theQueue = new myQueue(stack1, stack2);
 
-            else if (a == 'd'){
-                myStack->deleteNode();
-            }
-
-            else if(a == 'm'){
-                std::cout << myStack->minval() << "\n";    
-            }
-            
-            else{std::cout << "Please enter a valid character (a,d,q)\n";}
-
-            flip = !flip;
-            a = 'q';
-        }
-        flip = !flip;
+    // Loading the stack with user's number
+    while(std::cin >> a){
+        theQueue->push(a);
     }
-    delete(myStack);
+    // loading onto stack1_reverse
+    theQueue->unloadStack1();
+    print_stack(theQueue->stack1_reverse);
     return 0;
 }
 
