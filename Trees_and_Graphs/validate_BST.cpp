7169572f60fcf8fcb2a6711ad56d22be7d0df27c
ryuparish <ryuparish1115@gmail.com>
2021-06-28 15:46:41 -0500

    Partially functional validate BST

--- Trees_and_Graphs/validate_BST.cpp
+++ Trees_and_Graphs/validate_BST.cpp
@@ -30,23 +30,50 @@ treeNode* createTree(int start, int end){
 // We can send a threshold along while searching the left and right side of the tree's children
 void inOrderValidate(treeNode*& root){
     if(root != NULL){
+
+        // DEBUG
+//        std::cout << "Current node: " << root->value << " left: ";
+//        if(root->left_child != NULL){
+//            std::cout << root->left_child->value << " ";
+//        }
+//        else{
+//            std::cout << 0 << " ";
+//        }
+//        // Now for checking the right child
+//        std::cout << "right: ";
+//        if(root->right_child != NULL){
+//            std::cout << root->right_child->value << " ";
+//        }
+//        else{
+//            std::cout << 0 << " ";
+//        }
+//        std::cout << "\n";
+//
         // Checking the left value (if larger)
         if(root->left_child != NULL && root->left_child->value > root->value){
-            std::cout << "Invalid node at root: " << root->value;
+            std::cout << "Invalid node at root: " << root->value << "\n";
         }
         inOrderValidate(root->left_child);
-        std::cout << root->value;
+        std::cout << root->value << "\n";
         inOrderValidate(root->right_child);
         // Checking the right value (if smaller)
         if(root->right_child != NULL && root->right_child->value < root->value){
-            std::cout << "Invalid node at root: " << root->value;
+            std::cout << "Invalid node at root: " << root->value << "\n";
         }
     }
 }
 
 int main(){
-    std::vector<int> myList = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    treeNode* myRoot = createTree(myList.front()-1, myList.size() + 1);
+    std::vector<int> myList = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    treeNode* myRoot = createTree(myList.front()-1, myList.size());
+    // Adding an erroneous node to the 5 node
+    treeNode* walker = myRoot;
+    walker = walker->right_child;
+    walker = walker->left_child;
+    treeNode* errorNode = new treeNode;
+    errorNode->value = 12;
+    walker->left_child = errorNode;
+    
     inOrderValidate(myRoot);
     std::cout << "\n";
     return 0;
