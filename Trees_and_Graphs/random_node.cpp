e837dfb59aab24100861ba27a9b0d8267a63dd87
ryuparish <ryuparish1115@gmail.com>
2021-07-03 14:20:47 -0500

    Added random node partially functional

--- Trees_and_Graphs/random_node.cpp
+++ Trees_and_Graphs/random_node.cpp
@@ -1,8 +1,12 @@
 #include <vector>
 #include <iostream>
 #include <unistd.h> // For debugging with print
+#include <stdlib.h> // For rand()
+#include <algorithm> // For remove
+
 struct treeNode{
     int value;
+    treeNode* parent = NULL;
     treeNode* left_child = NULL;
     treeNode* right_child = NULL;
 };
@@ -21,7 +25,8 @@ struct Tree{
     
     // In order print 
     void inOrderSearch(treeNode*& someRoot){
-        walker = someRoot;
+        // This cannot use the object walker because of the recursive contradictions
+        treeNode* walker = someRoot;
         if(walker != NULL){
             inOrderSearch(walker->left_child);
             std::cout << walker->value;
@@ -29,38 +34,78 @@ struct Tree{
         }
     }
 
-    transplant(node*& root, node*& node1, node*& node2){
+    // This frees/destroys node1 and replaces it with node2
+    void transplant(treeNode*& root, treeNode*& node1, treeNode*& node2){
         if(node1->parent == NULL){
             root = node2;
         }
-        else if(node1 == node1->parent->left){
-            node1->parent->left = node2;
+        else if(node1 == node1->parent->left_child){
+            node1->parent->left_child = node2;
         }
-        else{node1->parent->right == node2;}
+        else{node1->parent->right_child == node2;}
         if(node2 != NULL){
             node2->parent = node1->parent;
         }
     }
+
+    treeNode* randomNode(){
+        int randomNodeValue = num_list[std::rand() % num_list.size()];
+        return BinarySearch(this->root, randomNodeValue);
+    }
+        
+    // Gets the left most child
+    treeNode* smallestNode(treeNode*& root){
+        this->walker = root;
+        while(walker->left_child != NULL){
+            walker = walker->left_child;
+        }
+        return walker;
+    }
     
-    void treeDelete(node*& root, int targetValue){
-        treeNode* target_node = BinarySearch(root, targetValue);
-        if(target_node->left == NULL){
-            transplant(root, target_node, target_node->right);
+    void treeDelete(int targetValue){
+        treeNode* target_node = BinarySearch(this->root, targetValue);
+        if(target_node == NULL){
+            std::cout << "Node to delete does not exist\n";
+            return;
         }
-        else if(target_node->right == NULL){
-            transplant(root, target_node, target_node->left);
+
+        // Case in which one or both children are NULL from the target node to delete
+        if(target_node->left_child == NULL){
+            transplant(root, target_node, target_node->right_child);
         }
+        else if(target_node->right_child == NULL){
+            transplant(root, target_node, target_node->left_child);
+        }
+        // Both children are present
+        // Case in which this can occur
+        //              |
+        //              |
+        //
+        //              * <-- To be deleted
+        //             / \
+        //            /   \
+        //           *     *
+        //                / \
+        //               *   *
+        //               ^
+        //               ^ Node that needs to go in the empty spot due to ordering
+        //
+        //
         else{
-            node* smallest_left = treeMinimum(target_node->right);
+            treeNode* smallest_left = smallestNode(target_node->right_child);
             if(smallest_left->parent != target_node){
-                transplant(root, smallest_left, smallest_left->right);
-                smallest_left->right = target_node->right;
-                smallest_left->right->parent = smallest_left;
+                // Frees node that is the replacement(smallest_left)
+                transplant(root, smallest_left, smallest_left->right_child);
+                smallest_left->right_child = target_node->right_child;
+                smallest_left->right_child->parent = smallest_left;
             }
+            // Frees target node and loses it forever
             transplant(root, target_node, smallest_left);
-            smallest_left->left = target_node->left;
-            smallest_left->left->parent = smallest_left;
+            smallest_left->left_child = target_node->left_child;
+            smallest_left->left_child->parent = smallest_left;
         }
+        // We must assume there will be no duplicates in this case
+        std::remove(this->num_list.begin(), this->num_list.end(), targetValue);
     }
 
     treeNode* BinarySearch(treeNode*& someRoot, int targetValue){
@@ -74,12 +119,10 @@ struct Tree{
                 walker = walker->right_child;
             }
             else{
-                std::cout << "Value is in the tree\n";
                 treeNode* returnValue = walker;
                 return returnValue;
             }
         }
-        std::cout << "Value is not in the tree\n";
         return NULL;
     }
 
@@ -129,8 +172,14 @@ treeNode* createTree(std::vector<int>& num_list, int start, int end){
 
     treeNode* left_child = createTree(num_list, start, mid);
     child->left_child = left_child;
+    if(left_child != NULL){
+        left_child->parent = child;
+    }
     treeNode* right_child = createTree(num_list, mid, end);
     child->right_child = right_child;
+    if(right_child != NULL){
+        right_child->parent = child;
+    }
 
     return child;
 }
@@ -142,5 +191,20 @@ int main(){
     myTree->inOrderSearch(myTree->root);
     std::cout << "\n";
 
+    // Testing add
+    std::cout << "Adding the value 10\n";
+    myTree->insert(10);
+    myTree->inOrderSearch(myTree->root);
+    std::cout << "\n";
+
+    // Testing delete
+    std::cout << "Deleting the value 4\n";
+    myTree->treeDelete(4);
+    myTree->inOrderSearch(myTree->root);
+    std::cout << "\n";
+
+    // Testing Random Node
+    treeNode* randomNode = myTree->randomNode();
+    std::cout << "Here is the value of the random node: " << randomNode->value << "\n";
     return 0;
 }
